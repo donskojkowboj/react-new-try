2c12f74440ea66be41b72e4b51f86b472c477076
lek-tests <a.kovrigin.dev@gmail.com>
2025-03-13 17:34:10 +0700

    fix: учтены комменты

--- src/components/tooltip/tooltip.jsx
+++ src/components/tooltip/tooltip.jsx
@@ -3,18 +3,26 @@ import { useState } from 'react';
 import styles from './tooltip.module.scss';
 
 export const Tooltip = ({ text, icon, additionalClassname = '' }) => {
-  const [show, setShow] = useState(false);
+  const [isShow, setIsShow] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsShow(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsShow(false);
+  };
 
   const tooltipClassname = `${styles.tooltip} ${additionalClassname}`;
 
   return (
     <span
-      onMouseEnter={() => setShow(true)}
-      onMouseLeave={() => setShow(false)}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
       className={tooltipClassname}
     >
       <span className={styles.icon}>{icon}</span>
-      {show && <span className={styles.description}>{text}</span>}
+      {isShow && <span className={styles.description}>{text}</span>}
     </span>
   );
 };
